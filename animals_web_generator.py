b4d11d1cb9203597b93557e4300150f1e8563e46
Janis Löns <janis.loens@outlook.de>
2025-06-21 20:26:19 +0200

    Added docstrings and read_html function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,12 +1,17 @@
 import json
 
-def load_data(file_path):
+def load_data(file_path) -> list:
     """Load data from a JSON file."""
     with open(file_path, 'r') as handle:
         return json.load(handle)
     
-def get_data(data):
-    """Print data in a readable format."""
+def get_data(data) -> str:
+    """Generate a string with information about foxes.
+    Args:
+        data (list): A list of dictionaries containing animal data.
+    Returns:
+        str: A formatted string with information about each fox.
+    """
     foxes = ''
     for fox in data:   
         name = fox['name']
@@ -18,9 +23,22 @@ def get_data(data):
         except KeyError:
             foxes += f"Name: {name}\nDiet: {diet}\nLocation: {', '.join(location)}\n\n"
     return foxes
+
+def read_html(file_path) -> str:
+    """Read the content of an HTML file.
+    Args:
+        file_path (str): The path to the HTML file.
+    Returns:
+        str: The content of the HTML file.
+    """
+    with open(file_path, 'r') as file:
+        return file.read()
+
         
 
 
 
 animals_data = load_data('animals_data.json')
-print(get_data(animals_data))
+foxes_data = get_data(animals_data)
+html_data = read_html('animals_template.html')
+
